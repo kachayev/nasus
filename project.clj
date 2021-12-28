9c29d8d07442e0ee9c38317793f5de9ce12bc2b5
Andrew Foltz-Morrison <afoltzm@gmail.com>
2021-12-28 10:45:44 -0500

    Update log4j to 2.17.0 + slf4j-log4j12 to 1.7.32
    This commit updates log4j to a known safe version on the 2.x branch.
    
    This is not a safety-critical upgrade for nasus; the 1.x branch of log4j
    contained no vulnerability to CVE CVE-2021-44228, and CVE-2021-4104 only
    applies to applications using log4j 1.x versions with JMSAppender
    enabled, so there is no evidence that nasus HTTP servers were affected by these vulnerabilities.
    
    References:
    https://nvd.nist.gov/vuln/detail/CVE-2021-44228
    https://nvd.nist.gov/vuln/detail/CVE-2021-4104

--- project.clj
+++ project.clj
@@ -5,11 +5,12 @@
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.cli "0.4.1"]
-                 [org.slf4j/slf4j-log4j12 "1.7.26"]
-                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
-                                                    javax.jms/jms
-                                                    com.sun.jmdk/jmxtools
-                                                    com.sun.jmx/jmxri]]
+                 [org.slf4j/slf4j-log4j12 "1.7.32"]
+                 [org.apache.logging.log4j/log4j-core "2.17.0"
+                  :exclusions [javax.mail/mail
+                               javax.jms/jms
+                               com.sun.jmdk/jmxtools
+                               com.sun.jmx/jmxri]]
                  [aleph "0.4.7-alpha5"]
                  [org.apache.tika/tika-core "1.20"]]
   :main http.server
